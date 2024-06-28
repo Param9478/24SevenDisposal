f6c6e05e9627474201373b5f025aaee4b78754bd
param9478 <singh.parminder9478@gmail.com>
2024-06-28 12:13:09 -0600

    Added . @babel/plugin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 United Disposal Ltd. All rights reserved.</p>
+        <p>&copy; 2024 24Seven Disposal Ltd. All rights reserved.</p>
       </div>
     </footer>
   );
