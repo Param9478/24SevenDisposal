3a6bdb9365414e8924b9d1a5201f2d429b8158f9
param9478 <singh.parminder9478@gmail.com>
2024-07-01 20:42:33 -0600

    forms

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,90 +1,65 @@
-// import React from 'react';
-
-// const ContactForm = ({ contactFormRef }) => {
-//   return (
-//     <section
-//       id="contactForm"
-//       ref={contactFormRef}
-//       className="mt-5 py-20 bg-gray-800 text-white px-6"
-//     >
-//       <div className="container mx-auto text-center">
-//         <h2 className="text-3xl font-bold mb-5">Contact Us</h2>
-//         <p className="mb-5 text-sm">
-//           Have questions or need assistance? Fill out the form below and our
-//           team will get back to you shortly!
-//         </p>
-//         {/* Form with Netlify attributes */}
-//         <form
-//           name="contact"
-//           method="POST" // Specify POST method for Netlify forms
-//           // data-netlify="true" // Enable Netlify form handling
-//           className="max-w-lg mx-auto"
-//           netlify
-//         >
-//           <input type="hidden" name="form-name" value="contact" />{' '}
-//           {/* Hidden field for Netlify form name */}
-//           <div className="flex flex-wrap mx-2">
-//             <div className="w-full md:w-1/2 px-2 mb-4">
-//               <input
-//                 type="text"
-//                 name="name" // Name attribute for form data mapping
-//                 placeholder="Your Name"
-//                 className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
-//               />
-//             </div>
-//             <div className="w-full md:w-1/2 px-2 mb-4">
-//               <input
-//                 type="email"
-//                 name="email" // Name attribute for form data mapping
-//                 placeholder="Your Email"
-//                 className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
-//               />
-//             </div>
-//             <div className="w-full px-2 mb-4">
-//               <textarea
-//                 name="message" // Name attribute for form data mapping
-//                 placeholder="Your Message"
-//                 rows="4"
-//                 className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
-//               ></textarea>
-//             </div>
-//             <div className="w-full px-2 mb-4">
-//               <button
-//                 type="submit"
-//                 className="bg-orange-500 hover:bg-orange-600 text-white py-3 px-6 rounded-full inline-block transition duration-300"
-//               >
-//                 Send Message
-//               </button>
-//             </div>
-//           </div>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ContactForm;
-
 import React from 'react';
 
-const ContactForm = () => {
+const ContactForm = ({ contactFormRef }) => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <label>
-        Your Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Your Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Message:
-        <textarea name="message"></textarea>
-      </label>
-      <button type="submit">Send</button>
-    </form>
+    <section
+      id="contactForm"
+      ref={contactFormRef}
+      className="mt-5 py-20 bg-gray-800 text-white px-6"
+    >
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-5">Contact Us</h2>
+        <p className="mb-5 text-sm">
+          Have questions or need assistance? Fill out the form below and our
+          team will get back to you shortly!
+        </p>
+        {/* Form with Netlify attributes */}
+        <form
+          name="contact"
+          method="POST" // Specify POST method for Netlify forms
+          data-netlify="true"
+          className="max-w-lg mx-auto"
+          onSubmit="submit"
+        >
+          <input type="hidden" name="form-name" value="contact" />{' '}
+          {/* Hidden field for Netlify form name */}
+          <div className="flex flex-wrap mx-2">
+            <div className="w-full md:w-1/2 px-2 mb-4">
+              <input
+                type="text"
+                name="name" // Name attribute for form data mapping
+                placeholder="Your Name"
+                className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-2 mb-4">
+              <input
+                type="email"
+                name="email" // Name attribute for form data mapping
+                placeholder="Your Email"
+                className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
+              />
+            </div>
+            <div className="w-full px-2 mb-4">
+              <textarea
+                name="message" // Name attribute for form data mapping
+                placeholder="Your Message"
+                rows="4"
+                className="w-full bg-gray-700 text-white py-2 px-4 rounded-lg outline-none focus:bg-gray-600"
+              ></textarea>
+            </div>
+            <div className="w-full px-2 mb-4">
+              <button
+                type="submit"
+                className="bg-orange-500 hover:bg-orange-600 text-white py-3 px-6 rounded-full inline-block transition duration-300"
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
